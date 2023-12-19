3481fdcc60b16a9f060ba1b04ef4f141ac12f595
Seongju38 <ljg013@naver.com>
2023-12-19 23:21:32 +0900

    섹션 3. 훈훈한 Javascript ) 섹션 4 | 반복문 - JS로 HTML, CSS 조작 (1)

--- JAVASCRIPT-BASIC/D-Day-counter/script.js
+++ JAVASCRIPT-BASIC/D-Day-counter/script.js
@@ -1,5 +1,8 @@
 const messageContainer = document.querySelector("#d-day-message");
-messageContainer.textContent = "D-Day를 입력해 주세요.";
+const container = document.querySelector("#d-day-container");
+
+container.style.display = "none";
+messageContainer.innerHTML = "<h3>D-Day를 입력해 주세요.</h3>";
 
 const dateFormMaker = function () {
   const inputYear = document.querySelector("#target-year-input").value;
@@ -21,9 +24,9 @@ const counterMaker = function () {
   const remaining = (targetDate - nowDate) / 1000;
 
   if (remaining <= 0) {
-    console.log("타이머가 종료되었습니다.");
+    messageContainer.innerHTML = "<h3>타이머가 종료되었습니다.</h3>";
   } else if (isNaN(remaining)) {
-    console.log("유효한 시간대가 아닙니다.");
+    messageContainer.innerHTML = "<h3>유효한 시간대가 아닙니다.</h3>";
   }
 
   const remainingDate = Math.floor(remaining / 3600 / 24);
