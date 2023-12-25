f9d075911c5d1ec461bd2aa99275ab3e05d6d372
Seongju38 <ljg013@naver.com>
2023-12-26 02:06:46 +0900

    섹션 3. 훈훈한 Javascript ) 섹션 7 | 스코프, 호이스팅 - addEventListener (2)

--- JAVASCRIPT-BASIC/ToDo-list/script.js
+++ JAVASCRIPT-BASIC/ToDo-list/script.js
@@ -4,10 +4,17 @@ const createTodo = function () {
   const todoList = document.querySelector("#todo-list");
   const newLi = document.createElement("li");
   const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
+
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+  });
 
   newSpan.textContent = todoInput.value;
+  newLi.appendChild(newBtn);
   newLi.appendChild(newSpan);
   todoList.appendChild(newLi);
+  console.log(newLi);
   todoInput.value = "";
 };
 
