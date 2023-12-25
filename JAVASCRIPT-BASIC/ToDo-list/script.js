93752105dc394c6eeface87a73bd23670386b2f9
Seongju38 <ljg013@naver.com>
2023-12-25 21:37:25 +0900

    섹션 3. 훈훈한 Javascript ) 섹션 6 | DOM - JS로 HTML 태그 생성

--- JAVASCRIPT-BASIC/ToDo-list/script.js
+++ JAVASCRIPT-BASIC/ToDo-list/script.js
@@ -1,6 +1,8 @@
 const keyCodeCheck = function () {
   if (window.event.keyCode === 13) {
     const inputValue = document.querySelector("#todo-input").value;
-    console.log(inputValue);
+    const newLi = document.createElement("li");
+    const newSpan = document.createElement("span");
+    console.log(inputValue, newLi, newSpan); // console.log에는 잘 뜨지만 화면에는 추가가 되지 않음
   }
 };
