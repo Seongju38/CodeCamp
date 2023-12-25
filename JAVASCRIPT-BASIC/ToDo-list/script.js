3ce40ab649aea09e1e3e652d04b6cc4008cd2062
Seongju38 <ljg013@naver.com>
2023-12-26 01:33:24 +0900

    섹션 3. 훈훈한 Javascript ) 섹션 6 | DOM - appendChild (3)

--- JAVASCRIPT-BASIC/ToDo-list/script.js
+++ JAVASCRIPT-BASIC/ToDo-list/script.js
@@ -1,12 +1,14 @@
+const todoInput = document.querySelector("#todo-input");
+
 const keyCodeCheck = function () {
-  if (window.event.keyCode === 13) {
+  if (window.event.keyCode === 13 && todoInput.value !== "") {
     const todoList = document.querySelector("#todo-list");
-    const inputValue = document.querySelector("#todo-input").value;
-    const newLi = document.createElement("li"); // DOM Element의 구조 생성
+    const newLi = document.createElement("li");
     const newSpan = document.createElement("span");
 
-    newSpan.textContent = inputValue; // span 태그 안에 값 넣기
-    newLi.appendChild(newSpan); // li 태그 안의 하위 태그로 span 추가
-    todoList.appendChild(newLi); // ul 태그 안의 하위 태그로 li 추가
+    newSpan.textContent = todoInput.value;
+    newLi.appendChild(newSpan);
+    todoList.appendChild(newLi);
+    todoInput.value = "";
   }
 };
