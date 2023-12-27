343408e3d90211a57eec37071efd2274f3ddcf59
Seongju38 <ljg013@naver.com>
2023-12-28 08:33:38 +0900

    섹션 3. 훈훈한 Javascript ) 섹션 8 | 구조분해할당 - 구조분해할당 (1)

--- JAVASCRIPT-BASIC/ToDo-list/script.js
+++ JAVASCRIPT-BASIC/ToDo-list/script.js
@@ -37,7 +37,7 @@ const createTodo = function (storageData) {
 };
 
 const keyCodeCheck = function () {
-  if (window.event.keyCode === 13 && todoInput.value !== "") {
+  if (window.event.keyCode === 13 && todoInput.value.trim() !== "") {
     createTodo();
   }
 };
@@ -75,10 +75,10 @@ const weatherSearch = function (position) {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=136b7d9376206ce02c081574d8e87d14`
   ).then((res) => {
-    return res.json(); // then에서 return을 하면 또 다시 then을 받을 수 있음
-  }).then((json) => { // 이전 then에서 return한 data가 들어오게 됨 - res.json()이 시간이 오래 걸렸어서 제대로 동작할 때까지 기다려주기 위함
+    return res.json();
+  }).then((json) => {
     console.log(json.name, json.weather[0].description);
-  }).catch((err) => { // 요청이 제대로 이루어지지 않았을 경우 - then과 함께 항상 사용해주면 됨
+  }).catch((err) => {
     console.error(err);
   })
 };
