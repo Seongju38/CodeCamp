4824c4a7c47de7e8cdb6a472205ea1ff3d99f8da
Seongju38 <ljg013@naver.com>
2024-01-07 01:53:15 +0900

    섹션 9 | container-presenter

---
+++ FRONTEND/class/src/components/units/board/write/BoardWrite.presenter.js
@@ -0,0 +1,16 @@
+import { RedInput, BlueButton } from "./BoardWrite.styles";
+
+export default function BoardWriteUI(props) {
+  return (
+    <div>
+      <div>@@@@@@@@ 여기는 프리젠터입니다 @@@@@@@@</div>
+      <div>
+        작성자: <RedInput type="text" onChange={props.aaa} />
+        제목: <input type="text" onChange={props.bbb} />
+        내용: <input type="text" onChange={props.ccc} />
+        <BlueButton onClick={props.ddd}>GRAPHQL_API 요청하기</BlueButton>
+      </div>
+      <div>@@@@@@@@ 여기는 프리젠터입니다 @@@@@@@@</div>
+    </div>
+  );
+}
