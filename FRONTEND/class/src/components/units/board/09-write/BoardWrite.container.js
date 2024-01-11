ca9f8bb9b6404b76da397ea86dbbad4a70264169
Seongju38 <ljg013@naver.com>
2024-01-12 01:23:27 +0900

    섹션 12 | boards 수정 1편 : 게시글 등록하고 상세 보기 페이지로 이동

---
+++ FRONTEND/class/src/components/units/board/09-write/BoardWrite.container.js
@@ -0,0 +1,58 @@
+import { useMutation } from "@apollo/client";
+import { useState } from "react";
+import { 나의그래프큐엘셋팅 } from "./BoardWrite.queries";
+import BoardWriteUI from "./BoardWrite.presenter";
+import { useRouter } from "next/router";
+
+export default function BoardWrite(props) {
+  const router = useRouter();
+
+  const [writer, setWriter] = useState();
+  const [title, setTitle] = useState();
+  const [contents, setContents] = useState();
+
+  const [나의함수] = useMutation(나의그래프큐엘셋팅);
+
+  const onClickSubmit = async () => {
+    const result = await 나의함수({
+      variables: {
+        writer: writer,
+        title: title,
+        contents: contents,
+      },
+    });
+    console.log(result);
+    router.push(`/boards/${result.data.createBoard.number}`);
+  };
+
+  const onClickUpdate = () => {
+    // 여기서 수정하기 하자!!
+  };
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+  };
+
+  return (
+    <div>
+      <div>$$$$$$$$ 여기는 컨테이너입니다 $$$$$$$$</div>
+      <BoardWriteUI
+        onClickSubmit={onClickSubmit}
+        onClickUpdate={onClickUpdate}
+        onChangeWriter={onChangeWriter}
+        onChangeTitle={onChangeTitle}
+        onChangeContents={onChangeContents}
+        isEdit={props.isEdit}
+      />
+      <div>$$$$$$$$ 여기는 컨테이너입니다 $$$$$$$$</div>
+    </div>
+  );
+}
