274171dfb4c7169d8ca17c0dda36aa369869e085
Seongju38 <ljg013@naver.com>
2024-01-13 07:56:42 +0900

    섹션 12 | boards 수정 1편 : 수정 하기 페이지에서 수정 후 상세 보기 페이지로 이동 - 수정 하러 가기 버튼을 눌렀을 때 기존 값이 사라지는 문제점 / 수정 하고 싶은 항목이 한 개일 때 다른 항목들이 사라지는 문제점 있음

--- FRONTEND/class/src/components/units/board/09-write/BoardWrite.container.js
+++ FRONTEND/class/src/components/units/board/09-write/BoardWrite.container.js
@@ -1,17 +1,18 @@
 import { useMutation } from "@apollo/client";
 import { useState } from "react";
-import { 나의그래프큐엘셋팅 } from "./BoardWrite.queries";
+import { 나의그래프큐엘셋팅, UPDATE_BOARD } from "./BoardWrite.queries";
 import BoardWriteUI from "./BoardWrite.presenter";
 import { useRouter } from "next/router";
 
 export default function BoardWrite(props) {
   const router = useRouter();
 
-  const [writer, setWriter] = useState();
-  const [title, setTitle] = useState();
-  const [contents, setContents] = useState();
+  const [writer, setWriter] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
 
   const [나의함수] = useMutation(나의그래프큐엘셋팅);
+  const [updateBoard] = useMutation(UPDATE_BOARD);
 
   const onClickSubmit = async () => {
     const result = await 나의함수({
@@ -25,8 +26,17 @@ export default function BoardWrite(props) {
     router.push(`/section09/09-03-boards/${result.data.createBoard.number}`);
   };
 
-  const onClickUpdate = () => {
-    // 여기서 수정하기 하자!!
+  const onClickUpdate = async () => {
+    const result = await updateBoard({
+      variables: {
+        number: Number(router.query.number),
+        writer: writer,
+        title: title,
+        contents: contents,
+      },
+    });
+    console.log(result);
+    router.push(`/section09/09-03-boards/${result.data.updateBoard.number}`);
   };
 
   const onChangeWriter = (event) => {
