2289909aead15f63225c35b88e5d4f7c4ebe1438
Seongju38 <ljg013@naver.com>
2024-01-13 08:43:05 +0900

    섹션 12 | boards 수정 2편 : 수정 하러 가기 버튼을 눌렀을 때 기존 값 유지

---
+++ FRONTEND/class/src/components/units/board/09-write2/BoardWrite.container.js
@@ -0,0 +1,69 @@
+import { useMutation } from "@apollo/client";
+import { useState } from "react";
+import { 나의그래프큐엘셋팅, UPDATE_BOARD } from "./BoardWrite.queries";
+import BoardWriteUI from "./BoardWrite.presenter";
+import { useRouter } from "next/router";
+
+export default function BoardWrite(props) {
+  const router = useRouter();
+
+  const [writer, setWriter] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  const [나의함수] = useMutation(나의그래프큐엘셋팅);
+  const [updateBoard] = useMutation(UPDATE_BOARD);
+
+  const onClickSubmit = async () => {
+    const result = await 나의함수({
+      variables: {
+        writer: writer,
+        title: title,
+        contents: contents,
+      },
+    });
+    console.log(result);
+    router.push(`/section09/09-04-boards/${result.data.createBoard.number}`);
+  };
+
+  const onClickUpdate = async () => {
+    const result = await updateBoard({
+      variables: {
+        number: Number(router.query.number),
+        writer: writer,
+        title: title,
+        contents: contents,
+      },
+    });
+    console.log(result);
+    router.push(`/section09/09-04-boards/${result.data.updateBoard.number}`);
+  };
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+  };
+
+  return (
+    <div>
+      <div>$$$$$$$$ 여기는 컨테이너입니다 $$$$$$$$</div>
+      <BoardWriteUI
+        onClickSubmit={onClickSubmit}
+        onClickUpdate={onClickUpdate}
+        onChangeWriter={onChangeWriter}
+        onChangeTitle={onChangeTitle}
+        onChangeContents={onChangeContents}
+        isEdit={props.isEdit}
+        data={props.data} // undefined 이거나 data 이거나 둘 중 하나!
+      />
+      <div>$$$$$$$$ 여기는 컨테이너입니다 $$$$$$$$</div>
+    </div>
+  );
+}
