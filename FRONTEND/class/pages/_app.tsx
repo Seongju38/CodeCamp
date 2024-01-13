dce06b8811155f594f05026d66fa991d075305b1
Seongju38 <ljg013@naver.com>
2024-01-13 10:01:59 +0900

    섹션 13 | TS 기초 : js -> ts(x)로 변경

--- FRONTEND/class/pages/_app.tsx
+++ FRONTEND/class/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps } : AppProps) {
   const client = new ApolloClient({
     uri: "http://backend-example.codebootcamp.co.kr/graphql",
     cache: new InMemoryCache(), // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터를 임시로 저장해 놓기 ==> 나중에 더 자세히 알아보기
