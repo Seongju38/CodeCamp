1c2a7c9418184d4862be159b57e1c36a5a6bd736
Seongju38 <ljg013@naver.com>
2024-01-13 13:06:16 +0900

    섹션 13 | typescript-boards : boards의 js -> ts(x)로 변경

---
+++ FRONTEND/class/src/components/units/board/10-write/BoardWrite.container.tsx
@@ -0,0 +1,73 @@
+import { useMutation } from "@apollo/client";
+import { useState } from "react";
+import { 나의그래프큐엘셋팅, UPDATE_BOARD } from "./BoardWrite.queries";
+import BoardWriteUI from "./BoardWrite.presenter";
+import { useRouter } from "next/router";
+
+export default function BoardWrite(props) {
+  const router = useRouter();
+
+  const [writer, setWriter] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  const [나의함수] = useMutation(나의그래프큐엘셋팅);
+  const [updateBoard] = useMutation(UPDATE_BOARD);
+
+  const onClickSubmit = async () => {
+    const result = await 나의함수({
+      variables: {
+        writer: writer,
+        title: title,
+        contents: contents,
+      },
+    });
+    console.log(result);
+    router.push(
+      `/section10/10-02-typescript-boards/${result.data.createBoard.number}`
+    );
+  };
+
+  const onClickUpdate = async () => {
+    const myVariables = { number: Number(router.query.number) };
+    if (writer) myVariables.writer = writer;
+    if (title) myVariables.title = title;
+    if (contents) myVariables.contents = contents;
+
+    const result = await updateBoard({
+      variables: myVariables,
+    });
+    console.log(result);
+    router.push(
+      `/section10/10-02-typescript-boards/${result.data.updateBoard.number}`
+    );
+  };
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+  };
+
+  return (
+    <div>
+      <div>$$$$$$$$ 여기는 컨테이너입니다 $$$$$$$$</div>
+      <BoardWriteUI
+        onClickSubmit={onClickSubmit}
+        onClickUpdate={onClickUpdate}
+        onChangeWriter={onChangeWriter}
+        onChangeTitle={onChangeTitle}
+        onChangeContents={onChangeContents}
+        isEdit={props.isEdit}
+        data={props.data} // undefined 이거나 data 이거나 둘 중 하나!
+      />
+      <div>$$$$$$$$ 여기는 컨테이너입니다 $$$$$$$$</div>
+    </div>
+  );
+}
