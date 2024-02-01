7928c86f85b224b77841e14f144b9ae9354a077c
Seongju38 <ljg013@naver.com>
2024-02-02 06:21:20 +0900

    섹션 14 | stop-propagation : 이벤트 버블링에 의한 이벤트 전파 확인

--- FRONTEND/class/pages/section11/11-02-stop-propagation/index.tsx
+++ FRONTEND/class/pages/section11/11-02-stop-propagation/index.tsx
@@ -17,18 +17,32 @@ export default function StaticRoutingMovedPage() {
 
   console.log(data?.fetchBoards);
 
-  const onClickAlert = (event: MouseEvent<HTMLDivElement>) => {
-    alert(event.currentTarget.id + "님이 작성한 글입니다.");
+  // const onClickAlert = (event: MouseEvent<HTMLDivElement>) => {
+  //   alert(event.currentTarget.id + "님이 작성한 글입니다.");
+  // }
+
+  const qqq1 = () => {
+    alert("1번 클릭");
   }
-  
+  const qqq2 = () => {
+    alert("2번 클릭");
+  }
+  const qqq3 = () => {
+    alert("3번 클릭");
+  }
+  const qqq4 = () => {
+    alert("4번 클릭");
+  }
+
+
   return (
     <div>
       {data?.fetchBoards.map((el: any) => (
-        <div id={el.writer} onClick={onClickAlert}>
-          <span>
-            <input type="checkbox" />
+        <div id={el.writer} onClick={qqq1}>
+          <span onClick={qqq2}>
+            <input type="checkbox" onClick={qqq3}/>
           </span>
-          <span style={{ margin: "10px" }}>{el.number}</span>
+          <span style={{ margin: "10px" }} onClick={qqq4}>{el.number}</span>
           <span style={{ margin: "10px" }}>{el.title}</span>
           <span style={{ margin: "10px" }}>{el.writer}</span>
         </div>
